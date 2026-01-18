7763e3eae3003c48029315482e126121d1e8f42a
Curio <kartikeycode369@gmail.com>
2026-01-19 02:29:47 +0530

    CHANGING BALL Speeds and making it move

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from paddle import Paddle
 from ball import Ball
 import time
 
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
-screen.tracer(0, 0)
+screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
@@ -17,8 +18,13 @@ screen.onkey(r_paddle.move_down, "Down")
 screen.onkey(l_paddle.move_up, "w")
 screen.onkey(l_paddle.move_down, "s")
 game_is_on = True
+
+# render initial state once
+screen.update()
+
 while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-    ball.move()
+    ball.move()                # move first
+    screen.update()            # then draw the new position
+    time.sleep(ball.move_speed)  # slow loop according to ball speed
+
 screen.mainloop()
